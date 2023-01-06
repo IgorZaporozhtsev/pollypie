d75a72168085936f7b16e841965ce01a63fc6729
ihor <i.zaporozhtsev@gmail.com>
2023-01-06 11:35:28 +0200

    initial commit

---
+++ kitchen/src/main/java/com/zeecoder/kitchen/KitchenApplication.java
@@ -0,0 +1,11 @@
+package com.zeecoder.kitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KitchenApplication.class);
+    }
+}
