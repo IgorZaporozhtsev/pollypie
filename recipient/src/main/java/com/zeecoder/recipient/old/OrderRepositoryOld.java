c87864462d540d7a8076dc2bd983844a6622e41d
ihor <i.zaporozhtsev@gmail.com>
2023-01-28 08:42:50 +0200

    Revert "refactoring"
    This reverts commit cae1b474

---
+++ recipient/src/main/java/com/zeecoder/recipient/old/OrderRepositoryOld.java
@@ -0,0 +1,13 @@
+/*
+package com.zeecoder.recipient;
+
+import com.zeecoder.domains.old.ClientOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface OrderRepository extends JpaRepository<ClientOrder, UUID> {
+}
+*/
