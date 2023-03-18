c02ac5ea755f52c0d2bbf8ff827bb4bbeb69cb0c
ihor <i.zaporozhtsev@gmail.com>
2023-03-18 18:41:21 +0200

    small refactoring

--- recipient/src/main/java/com/zeecoder/recipient/security/JWTService.java
+++ recipient/src/main/java/com/zeecoder/recipient/security/JWTService.java
@@ -28,6 +28,15 @@ public class JWTService {
         return claimsResolver.apply(claims);
     }
 
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
     public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
     }
@@ -58,16 +67,6 @@ public class JWTService {
     private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
-
-    private Claims extractAllClaims(String token) {
-        return Jwts
-                .parserBuilder()
-                .setSigningKey(getSignInKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
-
     private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
