18a5884a6b4a7653980a0ace9728d6a4cb5b9a27
ihor <i.zaporozhtsev@gmail.com>
2023-04-04 14:31:08 +0300

    add test data

---
+++ recipient/src/main/resources/test_data/test_data.sql
@@ -0,0 +1,212 @@
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('99629192-90b5-4739-aead-41374c330d07', 'Apt 1917', 'mmulberry0@narod.ru', 'Matilde', 'Mulberry',
+        '+63 (174) 570-2436',
+        'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.',
+        null, 626);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('06017d05-503c-45d3-87bb-0457f70d20cd', 'Apt 599', 'phubatsch0@miibeian.gov.cn', 'Patricio', 'Hubatsch',
+        '+55 (295) 757-9313',
+        'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.',
+        null, 440);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('61fb1ce9-dae3-4dfa-b6d3-c1c03ecd72dc', 'PO Box 34599', 'gpeaddie1@xinhuanet.com', 'Gloriana', 'Peaddie',
+        '+86 (941) 200-8858',
+        'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.',
+        null, 16);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('6379f719-f321-434e-b7ab-59ae1edc033f', 'Room 1310', 'esare2@imageshack.us', 'Edithe', 'Sare',
+        '+55 (988) 854-6585',
+        'Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.',
+        null, 295);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('f6ce9af9-d074-40cb-971c-d8f700825f02', 'Apt 1422', 'mredmain3@nps.gov', 'Mirella', 'Redmain',
+        '+57 (357) 438-4891',
+        'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', null, 469);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('d6e2ea4b-3511-44eb-aa68-c51b8c0dff6a', 'Apt 165', 'ehourstan4@goodreads.com', 'Egon', 'Hourstan',
+        '+86 (956) 292-3150',
+        'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.',
+        null, 211);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('3270fb1b-e958-4384-a550-3332a5a5a3b3', 'Suite 2', 'aibberson5@tinyurl.com', 'Aluino', 'Ibberson',
+        '+7 (628) 501-6843',
+        'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.',
+        null, 354);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('81644ba6-5cfe-472c-8e09-1312710a8030', 'Apt 352', 'djerrans6@timesonline.co.uk', 'Dilan', 'Jerrans',
+        '+389 (172) 479-2136',
+        'In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.',
+        null, 777);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('c4601ec2-e14d-4812-8ed1-103853c94ecd', 'PO Box 37234', 'laslott7@fotki.com', 'Lillian', 'Aslott',
+        '+593 (870) 507-6483', 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.', null, 651);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('ee55d930-4a6e-48cf-8a14-b92bad34b528', 'Room 1759', 'sseifenmacher8@creativecommons.org', 'Sherwin',
+        'Seifenmacher', '+7 (964) 487-3259',
+        'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', null, 753);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('eb2f08fe-7f86-4766-abb0-2424cef5cde5', 'Apt 1110', 'pcockarill9@alibaba.com', 'Peterus', 'Cockarill',
+        '+57 (886) 881-6515', 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.', null,
+        491);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('86de1078-6213-4400-81c1-8372bfde61cc', 'Apt 124', 'bgutteridgea@foxnews.com', 'Berna', 'Gutteridge',
+        '+265 (236) 108-5415',
+        'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.', null, 239);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('77bf592e-ae19-4cb1-8443-c78a1d0ef130', 'Room 1284', 'hshearab@buzzfeed.com', 'Humfrey', 'Sheara',
+        '+62 (575) 538-7745',
+        'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.',
+        null, 823);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('c0315c97-a498-4be9-9321-10ed64e27b13', 'Apt 611', 'lbrockc@auda.org.au', 'Lucretia', 'Brock',
+        '+62 (144) 943-2448', 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', null, 900);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('5203ea1e-b988-4e15-ab80-f1d68e1f1f61', 'Suite 34', 'csalazard@edublogs.org', 'Corbin', 'Salazar',
+        '+963 (423) 383-2696', 'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.',
+        null, 434);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('6cbc48ce-7862-4fc9-bd38-b498bcaa27a8', 'PO Box 95837', 'rnencie@nih.gov', 'Reid', 'Nenci',
+        '+386 (139) 131-0744',
+        'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.',
+        null, 383);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('be1676a3-59a1-4044-8fd7-8043f861545e', 'Room 1298', 'lknewstubbf@cdc.gov', 'Libbey', 'Knewstubb',
+        '+86 (948) 624-3431',
+        'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.',
+        null, 14);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('87107bd8-cfab-45e8-9173-ce1d8224c5a5', 'PO Box 67156', 'dlaiseg@scientificamerican.com', 'Delano', 'Laise',
+        '+86 (143) 481-8138',
+        'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.',
+        null, 96);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('b9f4deee-0f23-4601-84c5-72c18a14c21a', 'PO Box 89464', 'jattenboroughh@mit.edu', 'Jayme', 'Attenborough',
+        '+86 (347) 829-8071', 'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.',
+        null, 332);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('941c82d6-ed73-4dc8-8e34-9403889d9158', '10th Floor', 'ggalvini@dedecms.com', 'Giovanna', 'Galvin',
+        '+241 (581) 396-3685',
+        'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.',
+        null, 596);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('8698e0d9-7087-4f09-b435-03b1d33d262a', 'Suite 40', 'efirsbyj@creativecommons.org', 'Elsa', 'Firsby',
+        '+351 (459) 714-7910',
+        'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.',
+        null, 124);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('54c2aac7-8cfc-4472-b026-e5bf6d6cebe1', '17th Floor', 'jbrugsmak@forbes.com', 'Jessa', 'Brugsma',
+        '+52 (295) 102-5914',
+        'Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.',
+        null, 326);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('67edd435-ace7-4f35-aa99-7e97fc28d8d2', '12th Floor', 'dalyutinl@ebay.co.uk', 'Doris', 'Alyutin',
+        '+420 (791) 955-1127',
+        'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+        null, 702);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('5075b970-50da-4fc1-bc31-0e627bbcaece', '11th Floor', 'ggarem@marketwatch.com', 'Gisela', 'Gare',
+        '+375 (491) 210-8788', 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.', null, 465);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('8952ae8c-6801-40f4-8490-db04cc091e87', 'Room 639', 'ccressarn@eventbrite.com', 'Clyde', 'Cressar',
+        '+7 (143) 676-0140',
+        'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.',
+        null, 841);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('7aecb685-528b-40aa-b664-db2bae268981', 'Apt 132', 'sfollio@addthis.com', 'Sheelagh', 'Folli',
+        '+63 (725) 735-9714',
+        'Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.',
+        null, 632);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('0a49b4ed-b988-463b-a64e-b05859294779', '6th Floor', 'mrishworthp@omniture.com', 'Marlo', 'Rishworth',
+        '+263 (659) 956-2957',
+        'Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.',
+        null, 859);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('f2f47401-7da4-4e96-9d99-492bd5dee8ad', 'Apt 1541', 'kstorrahq@twitpic.com', 'Kacy', 'Storrah',
+        '+86 (904) 228-4250', 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', null, 684);
+insert into client_order (order_id, address, email, first_name, last_name, phone_number, description, state,
+                          total_price)
+values ('a2370dea-a57d-43ef-94cb-c20544a7fdcd', 'Suite 52', 'rwestwoodr@ucoz.com', 'Raquela', 'Westwood',
+        '+86 (656) 603-0896', 'Fusce consequat. Nulla nisl. Nunc nisl.', null, 967);
+
+
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('5491da46-6bb3-418e-b32e-9a4b03848f50', 1, 192, 'Pepper - Gypsy Pepper',
+        'a2370dea-a57d-43ef-94cb-c20544a7fdcd');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('84dd654a-3c19-4c15-8fb8-83a99f7ed641', 2, 658, 'Wine - Sawmill Creek Autumn',
+        'a2370dea-a57d-43ef-94cb-c20544a7fdcd');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('f8b6ef92-14ad-442a-b2c0-1d8964796af3', 3, 287, 'Wine - Delicato Merlot',
+        '0a49b4ed-b988-463b-a64e-b05859294779');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('cc8e7f47-2328-49f2-8011-7e4ad6f5caa5', 4, 810, 'Squeeze Bottle', '7aecb685-528b-40aa-b664-db2bae268981');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('83428e97-ad00-4b6b-96ee-50da281b6d37', 5, 520, 'Vinegar - White Wine', 'b9f4deee-0f23-4601-84c5-72c18a14c21a');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('181998b4-350f-4a41-8a70-1e90a2c3fe64', 6, 352, 'Crawfish', 'b9f4deee-0f23-4601-84c5-72c18a14c21a');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('a796f48d-fa55-4315-ba5c-36f36382447f', 7, 713, 'Carbonated Water - Cherry',
+        'b9f4deee-0f23-4601-84c5-72c18a14c21a');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('786b16fb-34fe-4b02-9674-6a477c6b5bd6', 8, 524, 'Pepper - Chilli Seeds Mild',
+        'c0315c97-a498-4be9-9321-10ed64e27b13');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('a26d15f0-2364-476b-ad83-5b45ab54302c', 9, 884, 'Tart Shells - Savory, 2',
+        'c0315c97-a498-4be9-9321-10ed64e27b13');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('9648b9a7-30f3-43ef-99bc-d35c7b9abeb0', 10, 372, 'Wine - Chateau Aqueria Tavel',
+        'c0315c97-a498-4be9-9321-10ed64e27b13');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('a3403d16-4ba4-4e35-b44a-e2b09ddd0c50', 11, 466, 'Roe - Lump Fish, Red',
+        'c0315c97-a498-4be9-9321-10ed64e27b13');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('ded6df45-19dc-49c2-83c4-3ee0583dd652', 12, 9, 'Beans - French', '99629192-90b5-4739-aead-41374c330d07');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('87cbef7a-ede5-43ac-a7fe-8bfd6b56c8ea', 13, 323, 'Wine - Magnotta - Bel Paese White',
+        '06017d05-503c-45d3-87bb-0457f70d20cd');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('1af6405b-fc30-4de6-92fe-9100e466cf15', 14, 495, 'Cassis', '61fb1ce9-dae3-4dfa-b6d3-c1c03ecd72dc');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('2bcffe99-8526-496c-93a6-bf892c61c601', 15, 414, 'Shrimp - 31/40', '6379f719-f321-434e-b7ab-59ae1edc033f');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('d2676104-84d7-448a-a140-b3e8d3c88be9', 16, 363, 'Pasta - Agnolotti - Butternut',
+        'd6e2ea4b-3511-44eb-aa68-c51b8c0dff6a');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('9c9ee13f-a08b-499a-986c-6c1c8af3b3f2', 17, 663, 'Pie Shell - 9', 'd6e2ea4b-3511-44eb-aa68-c51b8c0dff6a');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('9e527fe6-70b3-45d7-b714-4ab3cca02376', 18, 776, 'Cookie - Oreo 100x2', '3270fb1b-e958-4384-a550-3332a5a5a3b3');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('cd2b423c-4805-4248-99cc-0b7cc029dd80', 19, 467, 'Crush - Grape, 355 Ml',
+        '81644ba6-5cfe-472c-8e09-1312710a8030');
+insert into item (item_id, amount, item_price, name, fk_order_id)
+values ('af838d54-9fd7-45c7-9510-a32260ba8fcc', 20, 527, 'Egg Patty Fried', 'c0315c97-a498-4be9-9321-10ed64e27b13');
+
